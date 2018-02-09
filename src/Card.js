b13f7a0efae36f920d63875d612e844f2e47c448
Robert Wilson <youmufan@gmail.com>
2018-02-09 18:18:49 -0500

    added ternary operator to conditionally select style of the card.

--- src/Card.js
+++ src/Card.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class Card extends Component {
 
 render() {
-    let icon;
+    const classes = this.props.isLiked ? "fa fa-heart fa-3x" : "fa fa-heart-o fa-3x";
     return (
         <div className='col-md-3'>
             <img src={this.props.src} alt={this.props.caption} />
             <p>{this.props.caption}</p>
-            <i className={(this.props.isLiked ? icon="fa fa-heart fa-5x" : icon="fa fa-heart-o fa-3x")} style={{color: 'red'}} aria-hidden="true"></i>
+            <i className={classes} style={{color: 'red'}} aria-hidden="true"></i>
         </div>
         );
     }
